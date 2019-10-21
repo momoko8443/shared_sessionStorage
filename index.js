4ea2cd3ddf2c2cd9bc8817252c255c450a7f8442
jake zheng <jakezheng@jakes-MacBook-Pro.local>
2019-10-21 18:01:42 +0800

    init

---
+++ index.js
@@ -0,0 +1,87 @@
+class SharedSessionStorage{
+    constructor(){
+        if(!window){
+            throw new Error('this library just support browser platform');
+        }
+        this.interval = 5000;
+        window.addEventListener('DOMContentLoaded', (event) => {
+            this._init();
+        });
+
+    }
+    // config(cfg){
+
+    // }
+    setItem(name, value){
+        window.localStorage.setItem(name, value);
+    }
+
+    getItem(name){
+        return window.localStorage.getItem(name);
+    }
+
+    onChange(callback){
+        window.addEventListener('storage', (result)=>{
+            callback && callback(result);
+        });
+    }
+    _init(){
+        var tabName = window.sessionStorage.getItem('tabName');
+        if(!tabName){
+            //is refresh page
+            tabName = 'tab_' + Math.floor(Math.random() * new Date().getTime());
+            window.sessionStorage.setItem('tabName',tabName);
+            this._clearExpiredData();
+            this._updateTimestamp(tabName);
+        }
+        document.getElementById('tabName').innerText = tabName;
+        window.setInterval(()=>{
+            this._updateTimestamp(tabName);
+        },this.interval);
+    }
+    
+    _updateTimestamp(tabName){
+        var pool = JSON.parse(window.localStorage.getItem('sharedTabPool'));
+        var time_stamp = new Date().getTime();
+        pool[tabName] = time_stamp;
+        window.localStorage.setItem('sharedTabPool',JSON.stringify(pool));
+    }
+    _clearExpiredData(){
+        var pool = JSON.parse(window.localStorage.getItem('sharedTabPool'));
+        for(var key in pool){
+            var nowTime = new Date().getTime();
+            if(nowTime - pool[key] > 5000){
+                //is expired
+                delete pool[key];
+            }
+        }
+        if(Object.keys(pool).length === 0){
+            window.localStorage.clear();
+            window.localStorage.setItem('sharedTabPool','{}');
+        }
+    }
+
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    init();
+    renderData();
+});
+window.addEventListener('storage', (result)=>{
+    renderData();
+});
+// document.getElementById('storeBtn').addEventListener('click',(e)=>{
+//     updateLocalStorage();
+//     renderData();
+// });
+
+// function renderData(){
+//     var data = window.localStorage.getItem('sharedData');
+//     if(data){
+//         document.getElementById('sharedData').innerText = data;
+//     }
+// }
+// function updateLocalStorage(){
+//     var value = document.getElementById('message').value;
+//     window.localStorage.setItem('sharedData',value);
+// }
